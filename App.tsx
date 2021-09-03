f4dff531b7e5190563494e4f9015aac05ae90bbc
Gabriel Araujo <gabriel.houth@gmail.com>
2021-09-02 22:37:49 -0300

    feat: add icon, splash and styles

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import SplashScreen from 'react-native-splash-screen';
 
 export const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return <></>;
 };
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,21 @@
+export const theme = {
+  colors: {
+    background: '#1A2133',
+
+    titleColor100: '#1A2133',
+    titleColor0: '#F8F8F8',
+    span: '#FFBF6B',
+
+    placeholder: '#737373',
+    highlight: '#FFBF6B',
+
+    primaryDark: '#1A2133',
+    secondaryDark: '#EF8F40',
+  },
+
+  fonts: {
+    titleFont100: 'Inter-Bold',
+    titleFont50: 'Inter-Medium',
+    subtitleFont: 'Inter-Regular',
+  },
+};
